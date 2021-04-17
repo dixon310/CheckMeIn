2d004d24a359e93d4952d8c9112b9b49b2b759bb
dixon310 <dixonchiu22@hotmail.com>
2021-04-17 13:23:57 +1000

    Working version with Add, edit and view

--- app/src/main/java/au/edu/utas/chiush/assignment2/AddStudent.kt
+++ app/src/main/java/au/edu/utas/chiush/assignment2/AddStudent.kt
@@ -4,8 +4,11 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import au.edu.utas.chiush.assignment2.databinding.ActivityMainBinding
 import au.edu.utas.chiush.assignment2.databinding.AddStudentActivityBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -18,12 +21,11 @@ class AddStudent: AppCompatActivity() {
         ui = AddStudentActivityBinding.inflate(layoutInflater)
         setContentView(ui.root)
 
-        val studentID = intent.getIntExtra(STUDENT_INDEX, -1)
-        var studentObject = items[studentID]
+        val db = Firebase.firestore
+       // val studentID = intent.getIntExtra(STUDENT_INDEX, -1)
+       /// var studentObject = items[studentID]
 
         //database /*need to add data from this end*/
-
-
         //Add student
         ui.btnAddStudent.setOnClickListener {
             //Add data
@@ -32,22 +34,23 @@ class AddStudent: AppCompatActivity() {
                 var txtStudentID = ui.txtStudentID. text
 
                 val lotr = Student(
-                    studentName = txtStudentName.toString(),
-                    studentID = txtStudentID.toString()
+                        studentName = txtStudentName.toString(),
+                        studentID = txtStudentID.toString()
                 )
-                val db = Firebase.firestore
+
                 var studentsCollection = db.collection("students")
-                studentsCollection.document()
-                    .add(lotr)
-                    .addOnSuccessListener {
-                        Log.d(FIREBASE_TAG, "Document created with id ${it.id}")
-                        lotr.id = it.id
-                        finish()
-                    }
-                    .addOnFailureListener {
-                        Log.e(FIREBASE_TAG, "Error writing document", it)
-                    }
+                studentsCollection
+                        .add(lotr)
+                        .addOnSuccessListener {
+                            Log.d(FIREBASE_TAG, "Document created with id ${it.id}")
+                            lotr.id = it.id
+                            finish()
+                        }
+                        .addOnFailureListener {
+                            Log.e(FIREBASE_TAG, "Error writing document", it)
+                        }
             }else{
+                //Alert box
                 val builder = AlertDialog.Builder(this)
                 with(builder){
                     setTitle("Invalid input")
@@ -58,7 +61,7 @@ class AddStudent: AppCompatActivity() {
             }
 
         }
-    }
 
+    }
 
 }
