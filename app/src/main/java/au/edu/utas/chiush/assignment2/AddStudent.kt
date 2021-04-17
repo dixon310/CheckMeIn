56919d6438337c6ddf9be5361de8fb03070009c1
dixon310 <dixonchiu22@hotmail.com>
2021-04-17 17:41:33 +1000

    Working version with Add, edit and view

--- app/src/main/java/au/edu/utas/chiush/assignment2/AddStudent.kt
+++ app/src/main/java/au/edu/utas/chiush/assignment2/AddStudent.kt
@@ -15,35 +15,41 @@ import com.google.firebase.ktx.Firebase
 
 class AddStudent: AppCompatActivity() {
     private lateinit var ui: AddStudentActivityBinding
+    private lateinit var ui2: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui = AddStudentActivityBinding.inflate(layoutInflater)
         setContentView(ui.root)
 
+        ui2 = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
         val db = Firebase.firestore
        // val studentID = intent.getIntExtra(STUDENT_INDEX, -1)
-       /// var studentObject = items[studentID]
+       // var studentObject = items[studentID]
 
         //database /*need to add data from this end*/
         //Add student
         ui.btnAddStudent.setOnClickListener {
             //Add data
             if (ui.txtStudentName.text.isNotEmpty() && ui.txtStudentID.text.isNotEmpty()) {
-                var txtStudentName = ui.txtStudentName.text
-                var txtStudentID = ui.txtStudentID. text
+                val txtStudentName = ui.txtStudentName.text
+                val txtStudentID = ui.txtStudentID. text
 
                 val lotr = Student(
-                        studentName = txtStudentName.toString(),
-                        studentID = txtStudentID.toString()
+                        studentName = txtStudentName?.toString(),
+                        studentID = txtStudentID?.toString()
                 )
 
                 var studentsCollection = db.collection("students")
                 studentsCollection
                         .add(lotr)
+
                         .addOnSuccessListener {
                             Log.d(FIREBASE_TAG, "Document created with id ${it.id}")
                             lotr.id = it.id
+                            (ui2.myList.adapter as StudentAdapter).notifyDataSetChanged()
                             finish()
                         }
                         .addOnFailureListener {
@@ -62,6 +68,26 @@ class AddStudent: AppCompatActivity() {
 
         }
 
+    }
+
+    inner class StudentAdapter(private val students: MutableList<Student>): RecyclerView.Adapter<MainActivity.StudentHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainActivity.StudentHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: MainActivity.StudentHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+
+    }
+    override fun onResume(){
+        super.onResume()
+        ui2.myList.adapter?.notifyDataSetChanged()
     }
 
 }
